b1d91d087e632e59855e987974b96a230f129d4d
Harald Pehl <harald.pehl@gmail.com>
2014-04-24 14:50:43 +0200

    Add basic spray support

--- src/main/scala/io/bst/Boot.scala
+++ src/main/scala/io/bst/Boot.scala
@@ -1,20 +1,21 @@
 package io.bst
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, Actor, ActorSystem}
 import com.sksamuel.elastic4s.ElasticClient
 import io.bst.ext.ElasticSearch
 import io.bst.user.User
 import java.util.UUID
 import java.util.UUID.randomUUID
+import akka.io.IO
+import spray.can.Http
 
 
-/**
- * @author Harald Pehl
- */
-object Bst extends App {
-  val system = ActorSystem("bst")
-  val es = ElasticSearch(ElasticClient.local, User(randomUUID, "foobar", "foo@bar.com"))
+object Boot extends App {
+  implicit val system = ActorSystem("bst-core")
+  val api = system.actorOf(Props[Api], "api")
+  IO(Http) ! Http.Bind(api, interface = "localhost", port = 8080)
 
+//  val es = ElasticSearch(ElasticClient.local, User(randomUUID, "foobar", "foo@bar.com"))
 //  val filename = new File(".").getCanonicalPath + "/random.txt"
 //  val fileProvider = system.actorOf(FileProvider.props(filename, es), "fileProvider")
 //  QuartzSchedulerExtension(system).schedule("Every10Seconds", fileProvider, Tick)
